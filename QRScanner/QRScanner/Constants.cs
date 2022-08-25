08c4b056eefb1d22db4738219c2958e47f7e1dfd
Tathata <andre.yovera1@gmail.com>
2022-08-25 18:21:53 -0400

    Adding StringCipher and SQLite functions

---
+++ QRScanner/QRScanner/Constants.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace QRScanner
+{
+    public static class Constants
+    {
+        public const string StringCipherKey = "ksec2021";
+
+        private const string DatabaseFileName = "SQLite.db3";
+
+        public const SQLite.SQLiteOpenFlags Flags =
+            SQLite.SQLiteOpenFlags.ReadWrite |
+            SQLite.SQLiteOpenFlags.Create |
+            SQLite.SQLiteOpenFlags.SharedCache;
+
+        public static string DatabasePath
+        {
+            get
+            {
+                var basePath =
+                    Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+                return Path.Combine(basePath, DatabaseFileName);
+            }
+        }
+    }
+}
